95eae3b5ddbd3efc25dea6ce18ec9bbe8b18c51b
Mcube728 <83777213+Mcube728@users.noreply.github.com>
2021-06-15 00:24:13 +0530

    Updated Scripts

--- xArcher.sh
+++ xArcher.sh
@@ -14,8 +14,10 @@ echo
 echo " Arch Linux Post Installation Script "
 echo "_____________________________________"
 
+yes | sudo pacman -Syyu     # Upgrade the system before the applications so as not to run into any errors 
+
+#===================================================================
 # This section takes care of the xorg server which is responsible for handling the programs that handle Graphical User Interfaces(GUIs).
-yes | sudo pacman -Syyu 
 echo
 echo "INSTALLING XORG..."
 echo
@@ -25,73 +27,112 @@ echo
 echo "DONE INSTALLING XORG! :)"
 echo
 
-# This section takes care of the DE/WM section. I wanted a WM only setup, but you can change the script to install a DE/WM of your choice. I chose i3. 
+
+#===================================================================
+# This section takes care of the DE/WM section. In the original script, I wanted to go with a window manager only setup, however, I changed my mind, and decided to go with a desktop environment setup(this is the reason why the original script[Archer.sh] is lacking a bit. Once I get some more time on my hands, I'll update the i3 script as well). This script will install the XFCE DE. The original Archer script has i3 as the window manager. Of course, you can hack this script to use something like Gnome, KDE, LXDE and so on. I chose XFCE because it's simple and fast, plus it's easy on system resources too. 
 echo
 echo "INSTALLING DESKTOP ENVIRONMENT XFCE..."
 echo
-yes | sudo pacman -S xfce4 --noconfirm
-yes | sudo pacman -S xfce4-goodies --noconfirm
-yes | sudo pacman -S networkmanager --noconfirm
-yes | sudo pacman -S networkmanager-applet --noconfirm
-yes | sudo pacman -S engrampa --noconfirm
-yes | sudo pacman -S qdfview   --noconfirm
-yes | sudo pacman -S firefox  --noconfirm
-yes | sudo pacman -S lynx  --noconfirm
-yes | sudo pacman -S ffmpeg  --noconfirm
-yes | sudo pacman -S rxvt-unicode  --noconfirm
-yes | sudo pacman -S htop  --noconfirm
-yes | sudo pacman -S neofetch --noconfirm
-yes | sudo pacman -S git  --noconfirm
-yes | sudo pacman -S wget  --noconfirm
-yes | sudo pacman -S scrot--noconfirm
-yes | sudo pacman -S libreoffice --noconfirm
-yes | sudo pacman -S calibre --noconfirm
-yes | sudo pacman -S bashtop --noconfirm
-yes | sudo pacman -S lxappearance --noconfirm
-yes | sudo pacman -S gnome-themes-extra --noconfirm
-yes | sudo pacman -S urxvt-perls --noconfirm
-yes | sudo pacman -S adobe-source-han-sans-otc-fonts --noconfirm
-yes | sudo pacman -S noto-fonts-emoji --noconfirm
-yes | sudo pacman -S --needed base-devel git
-yes | sudo pacman -S gcolor2 --noconfirm
-yes | sudo pacman -S gimp --noconfirm
-yes | sudo pacman -S mousepad --noconfirm
-yes | sudo pacman -S hexchat --noconfirm
-yes | sudo pacman -S irssi --noconfirm
-yes | sudo pacman -S zip --noconfirm
-yes | sudo pacman -S unzip --noconfirm
-yes | sudo pacman -S unrar --noconfirm
-yes | sudo pacman -S vlc --noconfirm
-yes | sudo pacman -S xf86-input-libinput --noconfirm
+# The System-------------------------------------------------------- 
+sudo pacman -S linux-lts                                            # Long Term Support Linux Kernel(Secondary boot option)
+yes | sudo pacman -S xf86-input-libinput --noconfirm                # Trackpoint Drivers (I'm making this script with my thinkpad in mind. For those of y'all that are curious, it's a T470 with an i5)
+
+# The Desktop Environment-------------------------------------------
+yes | sudo pacman -S xfce4 --noconfirm                              # XFCE Desktop Environment
+yes | sudo pacman -S xfce4-goodies --noconfirm                      # XFCE Desktop Environment extras
+yes | sudo pacman -S networkmanager --noconfirm                     # Network Connection Manager 
+yes | sudo pacman -S networkmanager-applet --noconfirm              # System tray icon
+yes | sudo pacman -S engrampa --noconfirm                           # Archive Manager
+
+# Terminal Shizz----------------------------------------------------
+yes | sudo pacman -S rxvt-unicode  --noconfirm                      # Terminal Emulator
+yes | sudo pacman -S urxvt-perls --noconfirm                        # Perl Scripts for URXVT
+yes | sudo pacman -S wget  --noconfirm                              # The non-interactive network Downloader
+yes | sudo pacman -S curl --noconfirm                               # Remote content retrieval
+yes | sudo pacman -S scrot--noconfirm                               # Command Line based Screenshot utility
+yes | sudo pacman -S htop  --noconfirm                              # System Resource Monitor
+yes | sudo pacman -S bashtop --noconfirm                            # Another Resource Monitor. Looks better than htop in my opinion..
+yes | sudo pacman -S unrar --noconfirm                              # RAR Compression program
+yes | sudo pacman -S unzip --noconfirm                              # Zip Compression Program
+yes | sudo pacman -S zip --noconfirm                                # Zip Compression Program
+yes | sudo pacman -S neofetch --noconfirm                           # Shows system information in the Terminal(AKA an Arch Linux User's badge of honour XD)
+
+# Development-------------------------------------------------------
+yes | sudo pacman -S --needed base-devel git                        #                        
+yes | sudo pacman -S git                                            # Version Control System
+yes | sudo pacman -S gcc                                            # C/C++ Compiler
+yes | sudo pacman -S glibc                                          # C libraries
+yes | sudo pacman -S vim                                            # Text editor
+
+# Productivity------------------------------------------------------
+yes | sudo pacman -S libreoffice --noconfirm                        # LibreOffice Suite
+yes | sudo pacman -S mousepad --noconfirm                           # Graphical text editor
+yes | sudo pacman -S qdfview   --noconfirm                          # Tabbed PDF Viewer. Browsers make great pdf viewers too, so you can totally ditch this.
+
+# Disk Utilities----------------------------------------------------
+yes | sudo pacman -S autofs --noconfirm                             # Auto-mounter
+yes | sudo pacman -S gparted --noconfirm                            # Disk Utility
+
+# Graphics and Design-----------------------------------------------
+yes | sudo pacman -S gimp --noconfirm                               # Photo Editing
+yes | sudo pacman -S gcolor2 --noconfirm                            # Color Picker
+yes | sudo pacman -S inkscape --noconfirm                           # Vector Image Creation
+
+# Communications----------------------------------------------------
+yes | sudo pacman -S irssi --noconfirm                              # Terminal Based IRC Client
+yes | sudo pacman -S hexchat --noconfirm                            # Multi Format Chat Client
+yes | sudo pacman -S discord --noconfirm                            # Voice And text Chat
+
+# Internet/Web Tools------------------------------------------------
+yes | sudo pacman -S lynx  --noconfirm                              # Terminal Based Web Browser
+yes | sudo pacman -S firefox  --noconfirm                           # Web Browser
+
+# Fonts and Themes--------------------------------------------------
+yes | sudo pacman -S gnome-themes-extra --noconfirm                 # Extra themes. Contains the Adwaita-dark Theme. 
+yes | sudo pacman -S adobe-source-han-sans-otc-fonts --noconfirm    # Fonts for Languages like Chinese, Japanese, Vietnamese, Mandarin and so on and so forth. 
+yes | sudo pacman -S noto-fonts-emoji --noconfirm                   # Emoji Fonts
+
+#Miscellaneous------------------------------------------------------
+yes | sudo pacman -S calibre --noconfirm                            # Ebook Management
+yes | sudo pacman -S vlc --noconfirm                                # Video Player
+yes | sudo pacman -S ffmpeg  --noconfirm                            # Video Converter (Mental Outlaw has a great video on youtube on how you can record your screen from the terminal via ffmpeg!)
 
 echo
 echo "DONE INSTALLING DESKTOP ENVIRONMENT XFCE! :)"
 echo
 
+
+#===================================================================
 # This section now takes care of the Display Manager. I chose LightDM. Feel free to replace LightDM with your choice of Display Manager. 
 echo
 echo "INSTALLING DISPLAY MANAGER-LIGHTDM..."
 echo
-yes | sudo pacman -S lightdm  --noconfirm
-yes | sudo pacman -S lightdm-gtk-greeter  --noconfirm
-yes | sudo pacman -S lightdm-gtk-greeter-settings  --noconfirm
+yes | sudo pacman -S lightdm  --noconfirm                           # Display/Login Manager
+yes | sudo pacman -S lightdm-gtk-greeter  --noconfirm               # Gtk Greeter
+yes | sudo pacman -S lightdm-gtk-greeter-settings  --noconfirm      # Settings for Lightdm
 echo
 echo "DONE INSTALLING DISPLAY MANAGER-LIGHTDM! :)"
 echo
 
+
+#===================================================================
 # This section takes care of the audio components now. 
 echo 
 echo "INSTALLING AUDIO COMPONENTS..."
 echo
-yes | sudo pacman -S alsa-utils  --noconfirm
-yes | sudo pacman -S alsa-plugins  --noconfirm
-yes | sudo pacman -S pulseaudio  --noconfirm
-yes | sudo pacman -S pulseaudio-alsa  --noconfirm
-yes | sudo pacman -S pavucontrol  --noconfirm
-yes | sudo pacman -S volumeicon  --noconfirm
+yes | sudo pacman -S alsa-utils  --noconfirm        # ALSA(Advanced Linux Sound Architechure) Components
+yes | sudo pacman -S alsa-plugins  --noconfirm      # ALSA Plugins
+yes | sudo pacman -S pulseaudio  --noconfirm        # Pulse Audio sound Components
+yes | sudo pacman -S pulseaudio-alsa  --noconfirm   # ALSA configuration for pulse audio
+yes | sudo pacman -S pavucontrol  --noconfirm       # Pulse Audio volume control
+yes | sudo pacman -S volumeicon  --noconfirm        # System tray volume control
 
 echo
 echo "DONE INSTALLING AUDIO COMPONENTS! :)"
+
+
+#===================================================================
+# This section takes care of the components required for printing
 echo 
 echo "INSTALLING PRINTER COMPONENTS..."
 echo 
@@ -106,5 +147,6 @@ echo "DONE INSTALLING PRINTER COMPONENTS! :)"
 echo "======================================================================"
 sudo systemctl enable lightdm
 echo
-echo "DONE INSTALLING THE APPLICATIONS! YOU CAN NOW REBOOT THE SYSTEM AND THEN LAUNCH THE CONFIGURATION SCRIPT FOR THE WM AND TERMINAL. :)"
 echo
+echo "DONE INSTALLING THE APPLICATIONS! YOU CAN NOW LAUNCH THE CONFIGURATION SCRIPT FOR THE SYSTEM. :)"
+
--- xArcher_config.sh
+++ xArcher_config.sh
@@ -14,17 +14,29 @@ echo
 echo "   Arch Linux Configuration Script   "
 echo "_____________________________________"
 
+#------------------------------------------
 echo 
 echo "SETTING LAPTOP LID CLOSE TO SUSPEND..."
 sudo sed -i -e 's|[# ]*HandleLidSwitch[ ]*=[ ]*.*|HandleLidSwitch=suspend|g' /etc/systemd/logind.conf
 echo
 
+#------------------------------------------
+# I got this from Rick Ellis' ArcMatic Arch installation script. He installed an LTS(Long Term Support) Kernel and configured it as a second boot option. 
+echo
+echo "CONFIGURING LTS KERNEL AS A SECONDARY BOOT OPTION"
+sudo cp /boot/loader/entries/arch.conf /boot/loader/entries/arch-lts.conf
+sudo sed -i 's|Arch Linux|Arch Linux LTS Kernel|g' /boot/loader/entries/arch-lts.conf
+sudo sed -i 's|vmlinuz-linux|vmlinuz-linux-lts|g' /boot/loader/entries/arch-lts.conf
+sudo sed -i 's|initramfs-linux.img|initramfs-linux-lts.img|g' /boot/loader/entries/arch-lts.conf
+
+#------------------------------------------
 echo
 echo "CONFIGURING CUPS SERVICE DAEMON FOR PRINTING..."
 systemctl enable org.cups.cupsd.service
 systemctl start org.cups.cupsd.service
 echo
 
+#------------------------------------------
 echo
 echo "ENABLING NETWORK TIME PROTOCOL SO THE CLOCK WILL BE SET VIA NETWORK..."
 sudo ntpd -qg
@@ -32,19 +44,41 @@ sudo systemctl enable ntpd.service
 sudo systemctl start ntpd.service
 echo
 
+#------------------------------------------
 echo 
 echo "CONFIGGURING VIM..."
 cp -f vimrc ~/.vimrc
 echo "CONFIGURED VIM :)"
 echo
 
+#------------------------------------------
 echo
 echo "CONFIGURING URXVT TERMINAL EMULATOR"
 cp -f Xresources /home/mikhail/.Xresources
 xrdb ~/.Xresources
-echo "URxvt.geometry: 120x30" > ~/.Xdefaults
+echo "URxvt.geometry: 95x50" > ~/.Xdefaults
 echo "CONFIGURED URXVT TERMINAL EMULATOR! :)"
+echo
+
+#------------------------------------------
+echo "NETWORK CONFIGURATION"
+echo "Find your network adapter:"
+ip link
+echo
+echo "Enter your network adapter:" LINK
+echo 
+echo "DISABLING DHCP AND ENABLING NETWORK MANAGER DAEMON..."
+echo 
+sudo systemctl disable dhcpcd.service
+sudo systemctl stop dhcpcd.service
+sudo ip link set dev ${LINK} down
+sudo systemctl enable NetworkManager.service
+sudo systemctl start NetworkManager.service
+sudo ip link set dev ${LINK} up
+
+#------------------------------------------
 cp -r startpage ~/.config/
 echo
 echo
-echo "THANK YOU FOR USING ARCHER!! :)"
+echo "THANK YOU FOR USING ARCHER!!"
+echo "YOU CAN NOW REBOOT THE SYSTEM. :)"
